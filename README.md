2576c01e51419ef00bd1b9bb0c5b21b2b215a1c7
JulienRC <royonchalendard.julien@gmail.com>
2020-01-29 10:02:43 +0100

    remove project to a new start

--- README.md
+++
@@ -1,5 +0,0 @@
-# API_Security
-VET - Projet
-
-https://spring.io/tools -> Download Spring Tools 4 for Eclipse
-
--- src/main/java/resource/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package resource;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<User, Integer> {
-
-}
